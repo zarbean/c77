6b28cf3068607142ed4e5a759820003497a816cd
zarbean <105830417+zarbean@users.noreply.github.com>
2022-05-20 18:40:38 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("My first console project")
+
+var first_name = "Zara";
+var last_name = "Batool";
+
+var full_name = first_name.concat(last_name);
+console.log (full_name);
+
+var a = 10;
+var b = 5;
+
+var add = a + b ;
+console.log(add);
+
+var sub = a - b ;
+console.log(sub);
+
+var multiple = a * b ;
+console.log(multiple);
+
+var divide = a / b ;
+console.log(divide);
